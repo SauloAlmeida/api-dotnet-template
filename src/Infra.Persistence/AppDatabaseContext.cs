98b29a3ac35f25fc4ab3c1c7613fad21902cb12d
Saulo Almeida <saulohenrique786@gmail.com>
2023-06-28 23:25:02 -0300

    feat: add infra.persistence layer

---
+++ src/Infra.Persistence/AppDatabaseContext.cs
@@ -0,0 +1,21 @@
+﻿using Domain.Entity;
+using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+
+namespace Infra.Persistence
+{
+    public class AppDatabaseContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+
+        public AppDatabaseContext(DbContextOptions<AppDatabaseContext> options)
+            : base(options) { }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
---
+++ src/Infra.Persistence/Configuration/UserConfiguration.cs
@@ -0,0 +1,39 @@
+﻿using Domain.Entity;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infra.Persistence.Configuration
+{
+    internal class UserConfiguration : IEntityTypeConfiguration<User>
+    {
+        public void Configure(EntityTypeBuilder<User> builder)
+        {
+            builder.HasKey(e => e.Id);
+
+            builder.Property(e => e.Name)
+                   .IsRequired()
+                   .HasMaxLength(100);
+
+            builder.Property(e => e.LastName)
+                   .IsRequired()
+                   .HasMaxLength(100);
+
+            builder.Property(e => e.Email)
+                   .IsRequired()
+                   .HasMaxLength(100);
+
+            builder.Property(e => e.Status)
+                   .IsRequired()
+                   .IsRequired();
+
+            builder.Property(e => e.CreatedAt)
+                   .IsRequired();
+
+            builder.Property(e => e.Password)
+                   .IsRequired()
+                   .HasMaxLength(100);
+
+            builder.HasData(new User("", "", "", ""));
+        }
+    }
+}
